26a353e7dd2ad38cbb29dd15ea50b00fbe1a4b74
Karlo Pagtakhan <kidap@yahoo.com>
2016-01-04 13:52:16 -0500

    Add print function

--- TestGitHub/ViewController.swift
+++ TestGitHub/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("I can't wait to push this to GitHub")
     }
 
     override func didReceiveMemoryWarning() {
